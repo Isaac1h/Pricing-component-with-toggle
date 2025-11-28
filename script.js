881b8d4fb43a4e067f3d56095f9be1a2d353b47d
Isaac Hassani <isaacnew31@gmail.com>
2025-11-28 03:08:01 +0100

    Release: JS cleaned & toggle fixed at 1440px

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const toggle = document.querySelector('.toggle input');
 const prices = document.querySelectorAll('.price-section h1');
 
-toggle.addEventListener('change', () => {
-  const pricesList = toggle.checked 
-    ? ['$19.99', '$24.99', '$39.99']    // Monthly
-    : ['$199.99', '$249.99', '$399.99']; // Annually
-  
+function updatePrices() {
+  let pricesList;
+  if (toggle.checked === true) {
+    pricesList = ['$19.99', '$24.99', '$39.99'];
+  } else {
+    pricesList = ['$199.99', '$249.99', '$399.99'];
+  }
   prices.forEach((price, i) => price.textContent = pricesList[i]);
-});
+}
+
+updatePrices();
+
+toggle.addEventListener('change', updatePrices);
